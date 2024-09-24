7b9b944a1af23244ccd19f853a45e7151bfc8c2b
Florian Krause <florian.krause@etribes.de>
2024-09-24 10:29:13 +0200

    Fix wrong package loading

--- createApp.js
+++ createApp.js
@@ -1,78 +1,110 @@
-import degit from 'degit';
-import { copyFile, unlink as deleteFile, readFile, writeFile } from 'node:fs/promises';
-import { basename, join, resolve } from 'node:path';
-import colors from 'picocolors';
-import runCommandInDir from './helpers/runCommandInDir.js';
+import degit from "degit";
+import {
+  copyFile,
+  unlink as deleteFile,
+  readFile,
+  writeFile,
+} from "node:fs/promises";
+import { basename, join, resolve } from "node:path";
+import colors from "picocolors";
+import runCommandInDir from "./helpers/runCommandInDir.js";
 
-const createApp = async config => {
+const createApp = async (config) => {
   // Parse project directory and app name
   config.projectDirectory = resolve(config.projectDirectory);
   config.appName = basename(config.projectDirectory);
 
   // Download template/boilerplate
-  console.log('Downloading project template...');
+  console.log("Downloading project template...");
   try {
-    const emitter = degit('gitlab:etribes/velocity-boilerplate', { cache: false, force: true, verbose: true });
+    const emitter = degit("gitlab:etribes/velocity-boilerplate", {
+      cache: false,
+      force: true,
+      verbose: true,
+    });
     await emitter.clone(config.projectDirectory);
   } catch {
-    console.log(`${colors.red('Error!')} We were unable to download the project template.`);
+    console.log(
+      `${colors.red("Error!")} We were unable to download the project template.`,
+    );
     return;
   }
 
   // Update app name in package.json
-  const packageJsonText = await readFile(join(config.projectDirectory, 'package.json'), 'utf-8');
+  const packageJsonText = await readFile(
+    join(config.projectDirectory, "package.json"),
+    "utf-8",
+  );
   const packageJson = JSON.parse(packageJsonText);
   packageJson.name = config.appName;
-  await writeFile(path, JSON.stringify(packageJson, null, 2), 'utf-8');
+  await writeFile(path, JSON.stringify(packageJson, null, 2), "utf-8");
 
   // ESLint
   if (!config.eslint) {
-    await deleteFile(join(config.projectDirectory, '.eslintrc'));
+    await deleteFile(join(config.projectDirectory, ".eslintrc"));
     // TODO: Remove dependency
   }
 
   // Prettier
   if (!config.prettier) {
-    await deleteFile(join(config.projectDirectory, '.prettierrc'));
+    await deleteFile(join(config.projectDirectory, ".prettierrc"));
     // TODO: Remove dependency
   }
 
   // Create local .env
-  await copyFile(join(config.projectDirectory, 'example.env'), join(config.projectDirectory, '.env'));
+  await copyFile(
+    join(config.projectDirectory, "example.env"),
+    join(config.projectDirectory, ".env"),
+  );
 
   // Install dependencies
   if (!config.skipInstall) {
-    console.log('Installing dependencies...');
-    if (config.packageManager !== 'npm') {
-      await deleteFile(join(config.projectDirectory, 'package-lock.json'));
+    console.log("Installing dependencies...");
+    if (config.packageManager !== "npm") {
+      await deleteFile(join(config.projectDirectory, "package-lock.json"));
     }
-    await runCommandInDir(config.projectDirectory, `${config.packageManager} install`);
+    await runCommandInDir(
+      config.projectDirectory,
+      `${config.packageManager} install`,
+    );
   }
 
   // Git
   if (config.disableGit) {
-    await deleteFile(join(config.projectDirectory, '.gitignore'));
+    await deleteFile(join(config.projectDirectory, ".gitignore"));
   } else {
-    await runCommandInDir(config.projectDirectory, 'git init');
+    await runCommandInDir(config.projectDirectory, "git init");
   }
 
   // Success!
-  console.log(`${colors.green('Success!')} Created ${config.appName} at ${config.projectDirectory}`);
-  console.log('Inside that directory, you can run several commands:');
+  console.log(
+    `${colors.green("Success!")} Created ${config.appName} at ${config.projectDirectory}`,
+  );
+  console.log("Inside that directory, you can run several commands:");
   console.log();
-  console.log(colors.cyan(`  ${config.packageManager} ${config.packageManager === 'yarn' ? '' : 'run '}dev`));
-  console.log('    Starts the development server.');
+  console.log(
+    colors.cyan(
+      `  ${config.packageManager} ${config.packageManager === "yarn" ? "" : "run "}dev`,
+    ),
+  );
+  console.log("    Starts the development server.");
   console.log();
-  console.log(colors.cyan(`  ${config.packageManager} ${config.packageManager === 'yarn' ? '' : 'run '}build`));
-  console.log('    Builds the app for production.');
+  console.log(
+    colors.cyan(
+      `  ${config.packageManager} ${config.packageManager === "yarn" ? "" : "run "}build`,
+    ),
+  );
+  console.log("    Builds the app for production.");
   console.log();
   console.log(colors.cyan(`  ${config.packageManager} start`));
-  console.log('    Runs the built app in production mode.');
+  console.log("    Runs the built app in production mode.");
   console.log();
-  console.log('We suggest that you begin by typing:');
+  console.log("We suggest that you begin by typing:");
   console.log();
-  console.log(`  ${colors.cyan('cd')} ${config.projectDirectory}`);
-  console.log(`  ${colors.cyan(`${config.packageManager} ${config.packageManager === 'yarn' ? '' : 'run '}dev`)}`);
+  console.log(`  ${colors.cyan("cd")} ${config.projectDirectory}`);
+  console.log(
+    `  ${colors.cyan(`${config.packageManager} ${config.packageManager === "yarn" ? "" : "run "}dev`)}`,
+  );
 };
 
 export default createApp;
--- index.js
+++ index.js
@@ -1,39 +1,70 @@
 #!/usr/bin/env node
 
-import { Command } from 'commander';
-import { readFile } from 'node:fs/promises';
-import colors from 'picocolors';
-import prompts from 'prompts';
-import createApp from './createApp.js';
-import getPkgManager from './helpers/getPkgManager.js';
+import { Command } from "commander";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import { readFile } from "node:fs/promises";
+import colors from "picocolors";
+import prompts from "prompts";
+import createApp from "./createApp.js";
+import getPkgManager from "./helpers/getPkgManager.js";
 
-const packageJson = JSON.parse(await readFile('./package.json', 'utf-8'));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const packageJson = JSON.parse(
+  await readFile(`${__dirname}/package.json`, "utf-8"),
+);
 
 const program = new Command(packageJson.name)
-  .version(packageJson.version, '-v, --version', 'Output the current version of create-velocity-app.')
-  .argument('[directory]')
-  .usage('[directory] [options]')
-  .helpOption('-h, --help', 'Display this help message.')
+  .version(
+    packageJson.version,
+    "-v, --version",
+    "Output the current version of create-velocity-app.",
+  )
+  .argument("[directory]")
+  .usage("[directory] [options]")
+  .helpOption("-h, --help", "Display this help message.")
   // .option('--ts, --typescript', 'Initialize as a TypeScript project. (default)')
   // .option('--js, --javascript', 'Initialize as a JavaScript project.')
   // .option('--tailwind', 'Initialize with Tailwind CSS config. (default)')
-  .option('--eslint', 'Initialize with ESLint config. (default)')
-  .option('--prettier', 'Initialize with Prettier config. (default)')
-  .option('--i18n', 'Initialize with i18n setup. (default, disabling not yet implemented)')
-  .option('--basic-auth', 'Initialize with Basic Auth setup. (default, disabling not yet implemented)')
+  .option("--eslint", "Initialize with ESLint config. (default)")
+  .option("--prettier", "Initialize with Prettier config. (default)")
   .option(
-    '--openid-connect',
-    'Initialize with OpenID Connect demo integration. (default, disabling not yet implemented)'
+    "--i18n",
+    "Initialize with i18n setup. (default, disabling not yet implemented)",
+  )
+  .option(
+    "--basic-auth",
+    "Initialize with Basic Auth setup. (default, disabling not yet implemented)",
+  )
+  .option(
+    "--openid-connect",
+    "Initialize with OpenID Connect demo integration. (default, disabling not yet implemented)",
   )
   // .option('--import-alias <prefix/*>', 'Specify import alias to use (default "@/*").')
   // .option('--empty', 'Initialize an empty project.')
-  .option('--use-npm', 'Explicitly tell the CLI to bootstrap the application using npm.')
-  .option('--use-pnpm', 'Explicitly tell the CLI to bootstrap the application using pnpm.')
-  .option('--use-yarn', 'Explicitly tell the CLI to bootstrap the application using Yarn.')
-  .option('--use-bun', 'Explicitly tell the CLI to bootstrap the application using Bun.')
-  .option('--skip-install', 'Explicitly tell the CLI to skip installing packages.')
-  .option('--yes', 'Use defaults for unprovided options.')
-  .option('--disable-git', 'Skip initializing a git repository.')
+  .option(
+    "--use-npm",
+    "Explicitly tell the CLI to bootstrap the application using npm.",
+  )
+  .option(
+    "--use-pnpm",
+    "Explicitly tell the CLI to bootstrap the application using pnpm.",
+  )
+  .option(
+    "--use-yarn",
+    "Explicitly tell the CLI to bootstrap the application using Yarn.",
+  )
+  .option(
+    "--use-bun",
+    "Explicitly tell the CLI to bootstrap the application using Bun.",
+  )
+  .option(
+    "--skip-install",
+    "Explicitly tell the CLI to skip installing packages.",
+  )
+  .option("--yes", "Use defaults for unprovided options.")
+  .option("--disable-git", "Skip initializing a git repository.")
   .parse(process.argv);
 
 const defaultOptions = {
@@ -49,7 +80,7 @@ const defaultOptions = {
   // importAlias: '@/*',
   // empty: false,
   skipInstall: false,
-  disableGit: false
+  disableGit: false,
 };
 
 const customOptions = program.opts();
@@ -57,14 +88,14 @@ const customOptions = program.opts();
 customOptions.projectDirectory = program.args[0]?.trim();
 
 customOptions.packageManager = !!customOptions.useNpm
-  ? 'npm'
+  ? "npm"
   : !!customOptions.usePnpm
-  ? 'pnpm'
-  : !!customOptions.useYarn
-  ? 'yarn'
-  : !!customOptions.useBun
-  ? 'bun'
-  : getPkgManager();
+    ? "pnpm"
+    : !!customOptions.useYarn
+      ? "yarn"
+      : !!customOptions.useBun
+        ? "bun"
+        : getPkgManager();
 
 if (customOptions.yes && customOptions.projectDirectory) {
   createApp({ ...defaultOptions, ...customOptions });
@@ -73,10 +104,10 @@ if (customOptions.yes && customOptions.projectDirectory) {
   const response = await prompts(
     [
       {
-        message: 'What is your project named?',
-        name: 'projectDirectory',
-        type: !customOptions.projectDirectory ? 'text' : null,
-        initial: 'my-velocity-app'
+        message: "What is your project named?",
+        name: "projectDirectory",
+        type: !customOptions.projectDirectory ? "text" : null,
+        initial: "my-velocity-app",
       },
       // {
       //   type: customOptions.typescript === undefined ? 'toggle' : null,
@@ -95,53 +126,53 @@ if (customOptions.yes && customOptions.projectDirectory) {
       //   inactive: 'No'
       // },
       {
-        type: customOptions.eslint === undefined ? 'toggle' : null,
-        name: 'eslint',
-        message: `Would you like to use ${colors.blue('ESLint')}?`,
+        type: customOptions.eslint === undefined ? "toggle" : null,
+        name: "eslint",
+        message: `Would you like to use ${colors.blue("ESLint")}?`,
         initial: defaultOptions.eslint,
-        active: 'Yes',
-        inactive: 'No'
+        active: "Yes",
+        inactive: "No",
       },
       {
-        type: customOptions.prettier === undefined ? 'toggle' : null,
-        name: 'prettier',
-        message: `Would you like to use ${colors.blue('Prettier')}?`,
+        type: customOptions.prettier === undefined ? "toggle" : null,
+        name: "prettier",
+        message: `Would you like to use ${colors.blue("Prettier")}?`,
         initial: defaultOptions.prettier,
-        active: 'Yes',
-        inactive: 'No'
+        active: "Yes",
+        inactive: "No",
       },
       {
-        type: customOptions.i18n === undefined ? 'toggle' : null,
-        name: 'i18n',
-        message: `Would you like to include the ${colors.blue('i18n')} setup? (disabling not yet implemented)`,
+        type: customOptions.i18n === undefined ? "toggle" : null,
+        name: "i18n",
+        message: `Would you like to include the ${colors.blue("i18n")} setup? (disabling not yet implemented)`,
         initial: defaultOptions.i18n,
-        active: 'Yes',
-        inactive: 'No'
+        active: "Yes",
+        inactive: "No",
       },
       {
-        type: customOptions.basicAuth === undefined ? 'toggle' : null,
-        name: 'basicAuth',
-        message: `Would you like to include the ${colors.blue('Basic Auth')} setup? (disabling not yet implemented)`,
+        type: customOptions.basicAuth === undefined ? "toggle" : null,
+        name: "basicAuth",
+        message: `Would you like to include the ${colors.blue("Basic Auth")} setup? (disabling not yet implemented)`,
         initial: defaultOptions.basicAuth,
-        active: 'Yes',
-        inactive: 'No'
+        active: "Yes",
+        inactive: "No",
       },
       {
-        type: customOptions.openidConnect === undefined ? 'toggle' : null,
-        name: 'openidConnect',
+        type: customOptions.openidConnect === undefined ? "toggle" : null,
+        name: "openidConnect",
         message: `Would you like to include the ${colors.blue(
-          'OpenID Connect'
+          "OpenID Connect",
         )} demo integration? (disabling not yet implemented)`,
         initial: defaultOptions.openidConnect,
-        active: 'Yes',
-        inactive: 'No'
-      }
+        active: "Yes",
+        inactive: "No",
+      },
     ],
     {
       onCancel: () => {
         cancelled = true;
-      }
-    }
+      },
+    },
   );
   if (!cancelled) {
     const newCustomOptions = { ...customOptions, ...response };
